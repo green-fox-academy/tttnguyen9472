8f796c846e351c7da10412ada80b6161b4c32dfb
Nguyen Thanh Tung <tttnguyen.sport@gmail.com>
2020-11-26 16:53:16 +0100

    Add Function To Center exercise

---
+++ week-02/day-03/src/FunctionToCenter.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FunctionToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that draws a single line and takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a line from that point to the center of the canvas.
+        // Fill the canvas with lines from the edges, every 20 px, to the center.
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < 17; i++) {
+                DrawALine(graphics, x, y);
+                x = x + 20;
+        }
+        for (int i = 0; i < 17; i++) {
+            DrawALine(graphics, x, y);
+            y = y + 20;
+        }
+        for (int i = 0; i < 17; i++) {
+            DrawALine(graphics, x, y);
+            x = x - 20;
+        }
+        for (int i = 0; i < 17; i++) {
+            DrawALine(graphics, x, y);
+            y = y - 20;
+        }
+    }
+
+    public static void DrawALine(Graphics graphics, int x, int y) {
+        graphics.drawLine(x, y, WIDTH / 2, HEIGHT / 2);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
+
